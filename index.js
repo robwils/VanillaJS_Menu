d8b5fc37eb10f5918a6aa46fae2f370c3d3cd61e
robwils <robwils.fs@gmail.com>
2022-08-21 13:46:07 -0400

    3 files modified

--- index.js
+++ index.js
@@ -11,19 +11,28 @@ console.log('connected');
 
 const navIcon = document.getElementById('navIcon');
 const navMenu = document.querySelector('.menuContainer');
-
-console.log(navMenu.classList);
+const navLinks = document.querySelectorAll('.menuLink');
 
 navIcon.addEventListener('mouseover', openMenu);
 navMenu.addEventListener('mouseleave', closeMenu);
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('mouseenter', linkHover);
+});
 
 function openMenu() {
   console.log('clicked');
   navMenu.classList.toggle(`open`);
   console.log(navMenu.classList);
+  navIcon.style.color = 'white';
 }
 
 function closeMenu() {
   navMenu.classList.remove(`open`);
   console.log('clicked');
+  navIcon.style.color = 'black';
+}
+
+function linkHover() {
+  console.log('hovered link');
 }
